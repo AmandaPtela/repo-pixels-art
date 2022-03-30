83f53ab1717203926e9f90da0397e81af3de6786
Amanda Portela <mandaptela@gmail.com>
2022-03-29 21:55:16 -0300

    pegar cor e pintar quase ok

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-const cor = document.getElementsByTagName("article");
-const pixel = document.querySelectorAll(".pixel");
+const cores = document.querySelector('#color-palette');
+const pixels = document.querySelector('#pixel-board');
+const body = document.getElementsByTagName('body')
+const tirarSelect = cores.children[0].classList.remove('selected'); 
 
-console.log(cor);
-i = 0;
-for (; i < cor.length; i += 1){
-    cor[i].addEventListener('click', mudarCor)
-    pixel[i].style.backgroundColor="red";
-    function mudarCor(){
-        pixel[i].style.backgroundColor="";
-    }
+window.addEventListener('load', tirarSelect);
+
+
+function pegarCor(ev) {
+let preto = cores.children[0];
+let verde = cores.children[1];
+let rosa = cores.children[2];
+let azul = cores.children[3];
+
+	if(preto.className == 'color'){
+	  ev.target.classList.toggle('selected');
+	}
+
+	else if(verde.className == 'color'){    
+	  ev.target.classList.toggle('selected');
+	}
+
+	else if(rosa.className == 'color'){
+	  ev.target.classList.toggle('selected');
+	}
+	
+	else if(azul.className == 'color'){
+	  ev.target.classList.toggle('selected');
+	}
 }
-console.log(i)
+cores.addEventListener('click', pegarCor);
+
+function pintarPixel(ev) {
+
+	for (let i = 0; i < 26;){;
+		if (cores.children[0].classList.contains('selected')){
+			ev.target.classList.add('pixelPreto')
+				i +=1;
+		}
+		else if (cores.children[1].classList.contains('selected')){
+			ev.target.classList.add('pixelVerde');
+			i+=1;
+		}
+		else if (cores.children[2].classList.contains('selected')){
+			ev.target.classList.add('pixelRosa');
+			i+=1;
+		}
+		else if (cores.children[3].classList.contains('selected')){
+			ev.target.classList.add('pixelAzul');
+			i+=1;
+		}
+	}
+}
+pixels.addEventListener('click', pintarPixel);
